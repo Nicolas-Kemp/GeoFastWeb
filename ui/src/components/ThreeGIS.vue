8268242ae4b982e51c248633693420a54c355e69
Nicolas Kemp <nicolas@afrigis.co.za>
2023-05-19 14:05:30 +0200

    ThreeJS viewer tablemountain

--- ui/src/components/ThreeGIS.vue
+++ ui/src/components/ThreeGIS.vue
@@ -7,6 +7,8 @@ import * as THREE from 'three';
 import { MapControls } from 'three/addons/controls/MapControls.js';
 import axios from 'axios';
 
+import tableMountainImg from '@assets/image/tablemountain.jpeg'
+
 export default {
 name: 'ThreeJsExample',
 
@@ -37,6 +39,9 @@ methods: {
         console.log(error);
         }
     },
+
+    
+
 },
 
 mounted() {
@@ -48,7 +53,7 @@ mounted() {
         camera.updateProjectionMatrix();
 
         renderer.setSize( (window.innerWidth) , window.innerHeight );
-        renderer.setClearColor( 0xFFA03B, 0 );
+        renderer.setClearColor( 0x263740, 1 );
 
     }
 
@@ -60,7 +65,7 @@ mounted() {
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.setClearColor( 0xFFA03B, 0 );
+    renderer.setClearColor( 0x263740, 1);
     document.getElementById("webgl_id").appendChild(renderer.domElement);
 
     const geometry = new THREE.BoxGeometry( 1, 1, 1 );
@@ -69,7 +74,7 @@ mounted() {
     scene.add( cube );
 
     camera.position.z = 5;
-    const light = new THREE.AmbientLight( 0xFFFF00 ); // soft white light
+    const light = new THREE.AmbientLight( 0xFFFF00, 0.5 ); // soft white light
     scene.add( light );
 
     function animate() {
@@ -108,15 +113,33 @@ mounted() {
                                                 let geometry_aoi = new THREE.BufferGeometry();
                                                 geometry_aoi.setIndex(tableMountainDict.aoi_indices);
                                                 geometry_aoi.setAttribute( 'position', new THREE.BufferAttribute( new Float32Array(tableMountainDict.aoi_vertices), 3 ) );
-                                                geometry_aoi.setAttribute( 'color', new THREE.BufferAttribute( new Float32Array(tableMountainDict.aoi_colour), 3 ) );
+                                                // geometry_aoi.setAttribute( 'color', new THREE.BufferAttribute( new Float32Array(tableMountainDict.aoi_colour), 3 ) );
                                                 geometry_aoi.setAttribute( 'uv', new THREE.BufferAttribute( new Float32Array(tableMountainDict.aoi_uvs), 2 ) );
                                                 geometry_aoi.computeVertexNormals();
+                                                console.log(tableMountainImg)
 
-                                                // let texture = new THREE.TextureLoader().load('static/images/hermanus.jpg');
+                                                // instantiate a loader
+                                                var tc_fishriver_walk = {};
+                                                var loader = new THREE.TextureLoader();
 
-                                                const material_aoi = new THREE.MeshBasicMaterial({
-                                                                                vertexColors: true
-                                                                            });
+                                                // load a resource
+                                                loader.load(
+                                                    // resource URL
+                                                    tableMountainImg,
+                                                    // Function when resource is loaded
+                                                    function ( texture ) {
+                                                        // do something with the texture
+   
+                                                            tc_fishriver_walk = {
+                                                                            map: texture,
+                                                                            vertexColors: false
+
+                                                                    };
+                                                                                         // let texture = new THREE.TextureLoader().load('static/images/hermanus.jpg');
+                                                console.log(tc_fishriver_walk)                        
+                                                const material_aoi = new THREE.MeshBasicMaterial(
+                                                    tc_fishriver_walk
+                                                                            );
                                                 let aoi_map = new THREE.Mesh( geometry_aoi, material_aoi );
                                                 scene.add( aoi_map );
                                                 //aoi_map.material.map = texture;
@@ -126,12 +149,40 @@ mounted() {
 
                                                 aoi_map.material.needsUpdate = true;
 
+                                                const directLight_slected_area = new THREE.DirectionalLight( 0xFFFFFF, 0.2);
+                                                directLight_slected_area.position.set(tableMountainDict.aoi_lry,0.5,tableMountainDict.aoi_lrx+0.008);
+                                                directLight_slected_area.target.position.set(tableMountainDict.aoi_lry,0,tableMountainDict.aoi_lrx);
+                                                // scene.add( directLight_slected_area );
+                                                // scene.add(directLight_slected_area.target);
+
                                                 camera.position.x = tableMountainDict.aoi_lry;
                                                 camera.position.y = 0.00001;
                                                 camera.position.z = tableMountainDict.aoi_lrx;
 
                                                 controls.target.set(tableMountainDict.aoi_centroid[1],0.001,tableMountainDict.aoi_centroid[0]);
                                                 controls.update();
+                                                    },
+                                                    // Function called when download progresses
+                                                    function ( xhr ) {
+                                                        console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+                                                    },
+                                                    // Function called when download errors
+                                                    function ( xhr ) {
+                                                        console.log( 'An error happened' );
+                                                        if (img_fishriverwalk['image']===null) {
+                                                                console.log('texture is undefined');
+                                                                tc_fishriver_walk = {
+                                                                color: 0xA6A4A1,
+                                                                roughness: 3,
+                                                                metalness: 0.6,
+                                                                vertexColors: true
+                                                                        };
+                                                                    }
+                                                    }
+                                                );
+
+
+                           
                                                 });
 },
 };
--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,13 +4,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+import path from 'node:path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   server: {port:3000},
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@assets': path.resolve(__dirname, './src/assets')
     }
   }
 })
